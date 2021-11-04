1a435a39d7e5f8ae21a0fff71e848fb31cfa1a51
artemisia000 <misiapierobon@gmail.com>
2021-11-04 15:52:59 +0100

    specifiche cards

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 
 //crea la struttura dati delle card
 
-const cards [
+const cards = [
     {
-        image: 'image2',
+        image: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        profession: 'Founder & CEO',
+    },
+
+    {
+        image: 'img/angela-caroll-chief-editor.jpg',
         name: 'Angela Caroll',
         profession: 'Chef Editor',
     },
 
     {
-        image: 'image3',
+        image: 'img/walter-gordon-office-manager.jpg',
         name: 'Walter Gordon',
         profession: 'Office Manager',
     },
 
     {
-        image: 'image4',
+        image: 'img/angela-lopez-social-media-manager.jpg',
         name: 'Angela Lopez',
         profession: 'Social Media Menager',
     },
 
     {
-        image: 'image5',
+        image: 'img/scott-estrada-developer.jpg',
         name: 'Scott Estrada',
         profession: 'Developer',
     },
 
     {
-        image: 'image6',
+        image: 'barbara-ramos-graphic-designer.jpg',
         name: 'Barbara Ramos',
         profession: 'Graphic Designer',
     },
@@ -35,4 +41,35 @@ const cards [
 
 ];
 
-console.log(`cards`);
+console.log(cards);
+console.table(cards);
+
+//creare un contenitore per tutti i dati delle cards
+
+const teamContainer = document.querySelector('.team-container');
+
+//generare i markup delle con i dati delle cards
+
+for(let i = 0; i = cards.length ; i++){
+    let cardsItem = cards[i];
+    console.log(cardsItem);
+
+
+//aggiungere le specifiche alle cards
+
+
+
+    teamContainer.innerHTML += `
+    <div class="team-card">
+        <div class="card-image">
+          <img
+          src="img/wayne-barnett-founder-ceo.jpg"
+          alt="Wayne Barnett" />
+        </div>
+        <div class="card-text">
+          <h3>${cardsItem.name} </h3>
+          <p>${cardsItem.profession} </p>
+        </div>
+  </div>
+    `
+}
