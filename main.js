aac26239953261beec3abe7ca917deeedf10de93
artemisia000 <misiapierobon@gmail.com>
2021-11-04 14:46:09 +0100

    struttura dati cards

---
+++ main.js
@@ -0,0 +1,38 @@
+
+//crea la struttura dati delle card
+
+const cards [
+    {
+        image: 'image2',
+        name: 'Angela Caroll',
+        profession: 'Chef Editor',
+    },
+
+    {
+        image: 'image3',
+        name: 'Walter Gordon',
+        profession: 'Office Manager',
+    },
+
+    {
+        image: 'image4',
+        name: 'Angela Lopez',
+        profession: 'Social Media Menager',
+    },
+
+    {
+        image: 'image5',
+        name: 'Scott Estrada',
+        profession: 'Developer',
+    },
+
+    {
+        image: 'image6',
+        name: 'Barbara Ramos',
+        profession: 'Graphic Designer',
+    },
+
+
+];
+
+console.log(`cards`);
